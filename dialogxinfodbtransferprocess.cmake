990b44cc3e121786dd34d20e659d95e0a0d206b4
horsicq <horsicq@gmail.com>
2024-03-03 01:57:01 +0100

    Update file(s): 2024-03-03

--- dialogxinfodbtransferprocess.cmake
+++ dialogxinfodbtransferprocess.cmake
@@ -5,10 +5,15 @@ include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/xdialogprocess.cmake)
 set(DIALOGXINFODBTRANSFERPROCESS_SOURCES
     ${XDIALOGPROCESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogxinfodbtransferprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxinfodbtransferprocess.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxinfodbtransferprocess.ui
     ${CMAKE_CURRENT_LIST_DIR}/xinfodbtransfer.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xinfodbtransfer.h
     ${CMAKE_CURRENT_LIST_DIR}/xinfodb.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xinfodb.h
     ${CMAKE_CURRENT_LIST_DIR}/xinfodboptionswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xinfodboptionswidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xinfodboptionswidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/xinfomenu.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xinfomenu.h
 )
