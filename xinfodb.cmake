44bbb04ac0123539843df8905d1f30c004e89c95
horsicq <horsicq@gmail.com>
2024-12-09 00:06:49 +0100

    Update file(s): 2024-12-09

--- xinfodb.cmake
+++ xinfodb.cmake
@@ -1,8 +1,13 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../XScanEngine/xscanengine.cmake)
+if (NOT DEFINED XSCANENGINE_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../XScanEngine/xscanengine.cmake)
+    set(XINFODB_SOURCES ${XINFODB_SOURCES} ${XSCANENGINE_SOURCES})
+endif()
+
 # mb TODO XCapstone
 set(XINFODB_SOURCES
+    ${XINFODB_SOURCES}
     ${XSCANENGINE_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xinfodb.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xinfodb.h
