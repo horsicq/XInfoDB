6513011f12cf72dd4960caa8f146085eb2df0870
horsicq <horsicq@gmail.com>
2025-06-02 19:05:58 +0200

    Update file(s): 2025-06-02

--- xinfodb.h
+++ xinfodb.h
@@ -711,8 +711,7 @@ public:
 
     RECORD_INFO getRecordInfo(quint64 nValue, RI_TYPE riType = RI_TYPE_GENERAL);
     static QString recordInfoToString(RECORD_INFO recordInfo, RI_TYPE riType = RI_TYPE_GENERAL);
-    // void clearRecordInfoCache();
-    // RECORD_INFO getRecordInfoCache(quint64 nValue);
+
     QList<XBinary::MEMORY_REPLACE> getMemoryReplaces(quint64 nBase = 0, quint64 nSize = 0xFFFFFFFFFFFFFFFF);
 
     enum RT {
